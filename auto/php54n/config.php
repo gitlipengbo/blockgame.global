50babfbd99eed17ed1904b643f24f591b665371f
root <root@localhost.localdomain>
2019-04-15 11:04:44 +0800

    提交项目到暂存区

---
+++ auto/php54n/config.php
@@ -0,0 +1,9 @@
+<?php
+$serverdk='8001';
+$host= '127.0.0.1';
+$username='root';
+$password='9b648';
+$dbname='niuniu';
+$charset = 'utf8mb4';
+$machine_http = 'http://127.0.0.1:5432/';
+$url='epp3.wpjjt.com.cn';
